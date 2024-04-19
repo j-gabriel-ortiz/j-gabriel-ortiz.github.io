f7d8bddeb2a19df071cf0bfaa53bf15ae02bac5b
J Gabriel Ortiz <ojgabriel.88@gmail.com>
2024-04-18 21:03:27 -0300

    Se agregaron los links al JS con target blank

--- script.js
+++ script.js
@@ -4,19 +4,24 @@ const copyBtn = document.getElementById('copyClipboard');
 const projectsMakes = [
   { name: 'E-commerce (building🚫)',
   imagen: 'img/Working.jpg',
-  repo: 'https://github.com/j-gabriel-ortiz/CodoACodo-C24261-Grupo13/tree/Gaby' },
+  code: 'https://github.com/j-gabriel-ortiz/CodoACodo-C24261-Grupo13/tree/Gaby',
+  repo: '#' },
   { name: 'ToDo List',
   imagen: 'img/Lista-de-Tareas.jpg',
-  repo: 'https://github.com/j-gabriel-ortiz/To-Do-List-HTML-CSS-JavaScript' },
+  code: 'https://github.com/j-gabriel-ortiz/To-Do-List-HTML-CSS-JavaScript',
+  repo: 'https://j-gabriel-ortiz.github.io/To-Do-List-HTML-CSS-JavaScript/' },
   { name: 'Hangman',
   imagen: 'img/Ahorcado.jpg',
-  repo: 'https://github.com/j-gabriel-ortiz/PlayHangman-Pyton-Tkinter' },
+  code: 'https://github.com/j-gabriel-ortiz/PlayHangman-Pyton-Tkinter',
+  repo: 'https://j-gabriel-ortiz.github.io/PlayHangman-Pyton-Tkinter/'},
   { name: 'Encryptor',
   imagen: 'img/Encriptador.jpg',
-  repo: 'https://github.com/j-gabriel-ortiz/Encriptador-HTML5-CSS-JavaScript' },
+  code: 'https://github.com/j-gabriel-ortiz/Encriptador-HTML5-CSS-JavaScript',
+  repo: 'https://j-gabriel-ortiz.github.io/Encriptador-HTML5-CSS-JavaScript/' },
   { name: 'Snake Game',
   imagen: 'img/Snake-Game.jpg',
-  repo: 'https://github.com/j-gabriel-ortiz/Snake-Game-HTML-CSS-JavaScript' }
+  code: 'https://github.com/j-gabriel-ortiz/Snake-Game-HTML-CSS-JavaScript',
+  repo: 'https://j-gabriel-ortiz.github.io/Snake-Game-HTML-CSS-JavaScript/' }
 ];
 
 
@@ -28,8 +33,8 @@ function showProjects () {
             <h3>${project.name}</h3>
             <img class='img-project' src='${project.imagen}' alt='${project.name}'>
             <div class='container-link'>
-                <a href=''><img src='img/globe-solid.svg' alt='World icon'>Project</a>
-                <a href='${project.repo}' target="_blank"><img src='img/github.svg' alt='Git Hub icon'>Code</a>
+                <a href='${project.repo}' target="_blank"><img src='img/globe-solid.svg' alt='World icon'>Project</a>
+                <a href='${project.code}' target="_blank"><img src='img/github.svg' alt='Git Hub icon'>Code</a>
             </div>
         `
         sectionProjects.append(divProject)
